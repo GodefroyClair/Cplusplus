12128606538f8ab1546d601a5f470a88e5d3a998
GodefroyClair <godefroy.clair@gmail.com>
2013-07-23 18:42:11 +0200

    first send

---
+++ FirstProject/Hello.cpp
@@ -0,0 +1,18 @@
+/*
+ * Hello.cpp
+ *
+ *  Created on: 27 juin 2013
+ *      Author: Claudine
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+   cout << "it works again!" << endl;
+   return 0;
+}
+
+
+
---
+++ TestC/test1.c
@@ -0,0 +1,8 @@
+/*
+ * test1.c
+ *
+ *  Created on: 22 juil. 2013
+ *      Author: Claudine
+ */
+
+
